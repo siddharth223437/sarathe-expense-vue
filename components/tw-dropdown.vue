32fa5cbefeb45d76c632851424a85b40b76430e7
siddharth2223437 <siddharthsarathe.nmims@gmail.com>
2020-12-12 19:37:18 -0600

    Initial commit

---
+++ components/tw-dropdown.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="relative">
+    <input type="text" class="form-control">
+    <div class="absolute top-0 right-0 px-3 py-2">
+      <svg class="h-6 w-6 text-gray-400" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M19 9l-7 7-7-7" />
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "tw-select",
+  data:()=>({
+
+  }),
+
+}
+</script>
+
+<style scoped>
+
+</style>
---
+++ pages/expense/add.vue
@@ -0,0 +1,13 @@
+<template>
+$END$
+</template>
+
+<script>
+export default {
+name: "add"
+}
+</script>
+
+<style scoped>
+
+</style>
